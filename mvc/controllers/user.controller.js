46823732b53f276bae3ed23d551ba7ef1130409f
gfrancoa <gfrancoa@outlook.com>
2021-11-02 21:19:31 -0500

    creando metodo list, creando package.json

--- mvc/controllers/user.controller.js
+++ mvc/controllers/user.controller.js
@@ -2,20 +2,27 @@ const ctrlUser = {},
 User = require('../models/user'),
 message = require('../config/message')
 
-ctrlUser.create = async(req, res) =>{
-  const newUser =  new User({
+ctrlUser.create = async (req, res) => {
+  const newUser = new User({
     name: req.body.name,
     phone: req.body.phone,
     password: req.body.password,
     document: req.body.document,
-    addres:{ 
-       street: req.body.addres.street,
-       kra: req.body.addres.kra,
-       number_house: req.body.addres.number_house
-    }
-  })
-  await newUser.save()
-  res.json({msg: message.success, status: true })
-}
+    addres: {
+      street: req.body.addres.street,
+      kra: req.body.addres.kra,
+      number_house: req.body.addres.number_house,
+    },
+  });
+  await newUser.save();
+  res.json({ msg: message.success, status: true });
+};
+
+ctrlUser.list = async (req, res) => {
+  const users = await User.find();
+  console.log("users", users);
+
+  res.json(users);
+};
 
 module.exports =  ctrlUser
--- mvc/routes/user.route.js
+++ mvc/routes/user.route.js
@@ -3,6 +3,7 @@ router =  express.Router(),
 userCtrl = require('../controllers/user.controller')
 
 router.post('/', userCtrl.create)
+router.get("/", userCtrl.list);
 //router.put('/', userCtrl.update)
 
 
