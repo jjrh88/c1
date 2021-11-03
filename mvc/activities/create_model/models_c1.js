69efcdac574a0120b641913679454c905fd39992
jjrenter <jjrenter@bancolombia.com.co>
2021-11-03 18:59:55 -0500

    se crea carpeta activities

---
+++ mvc/activities/create_model/models_c1.js
@@ -0,0 +1,240 @@
+CRISTIAN ORLANDO ROMERO ACOSTA - products 
+CRISTIAN DAVID SAINEA CÉSPEDES - grants - permisos
+ {
+     user:"javier",
+     profile:{
+        name:"",
+        img:""
+        legenda:""
+     },
+     status: false,
+     created:
+     update:
+ }
+
+CARLOS ANDRES FANDIÑO RUEDA- - roles 
+  {
+      name:"",
+      status,
+      modulos:[
+        { name: "" }
+      ],
+       created:
+       update:
+  }
+EDGAR LEONARDO RODRIGUEZ VASQUEZ - inventario
+ {
+     date_generation:
+     articles:[
+         { name: "pala", cant_init:20 , stock: 10},
+         { name: "alicarte", cant_init:400,stock: 200 }
+     ],
+    status,
+    created:
+    update:
+}
+KELLY JOHANA VERGARA TORRES - facturacion
+ {
+   number_invoice: "",
+   cufe: "",
+   mix: 0,
+   max: 1000
+   status,
+   created:
+   update:
+   nit:"",
+   email: "",
+   phone:
+ }
+EDWIN GERMAN VILLALBA GONZALEZ - ventas
+{
+   idproduct: "",
+   cant:,
+   status,
+   created:
+   update:
+   type:"inventario o registrado"
+}
+
+LINA MARÍA RODRÍGUEZ RAMÍREZ - categorias
+{
+   name:"",
+   status:
+   created:
+   update:
+   color:
+   products:[
+     { name: "a1"},
+     { name: "a2"}
+  ]
+}
+ANDRES FELIPE GIRALDO - mensajes - chat 
+{
+  message:"a2"
+  from:"javier"
+  to:"juan",
+   status:
+   created:
+   update:
+   color: "",
+   type_message: "text/voz"
+   files:{
+      { name:"png"},
+      { name:"jpg"},
+      { name:"mp4"}
+   }
+}
+MARCOS ORTIZ - tipos de documentps
+{
+  name: "CC/TI/NIT/PASS",
+  pais:"colombia",
+  status:
+  created:
+  update:
+  total_register: 4000
+}
+JHON FAVER MACHADO ALAPE - departamentos
+{
+ name: "",
+ municipios:[
+  { name:"buenaventura"},
+  { name:"milan"},
+  { name:"yotoco"}
+ ],
+   status:
+  created:
+  update:
+}
+FRANCISCO JAVIER TORRES VILLAMOR - municipios
+{
+ name: "",
+ departamentos:[
+  { name:"valle"},
+  { name:"caqueta"},
+  { name:"antioquia"}
+ ],
+   status:
+  created:
+  update:
+}
+ALEJANDRO RIVAS LOPEZ - notificaciones
+{
+  type:"push/email/carta fisica/sms",
+  "description":""
+  addres: "",
+  phone:
+  status:
+  created:
+  update:
+}
+JORGE ALBERTO RODRIGUEZ PAEZ - location
+{
+ latitud:
+ longitud:
+ mtr:4000
+ ip: "",
+ continente:
+  status:
+  created:
+  update:,
+  satetlite: "google/aws/azure etc"
+}
+JOHN JAIRO ORJUELA ESPINOSA - paises
+{
+ name:"",
+ poblacion:""
+ estratos:[
+   1,2,3,5...
+ ],
+  status:
+  created:
+  update:,
+  cnt_department:300
+  cnt_capitales:900
+}
+DIANA GARCIA SANDOVAL - sistemas logs
+{
+  
+}
+DANIEL CAMILO RAMIREZ MARTINEZ - copias de seguridad
+{
+  os:"linux/mac/winserver",
+  application:"cobros/reporteria/custodias/control de accesos"
+  status:
+  created:
+  update:,
+  sharplex:"dell/redhat/oracle linux"
+}
+SANTIAGO HERRERA GOMEZ - perfiles
+{
+  avatar:"urlimage",
+  social: "facebook/twitter/twenty/instagram/linkeding",
+  followers:300,
+  followersbyCountry:500,
+  comments:{
+    cnt:50,
+    users:[
+     {  name: "" } 
+    ]
+   }
+  status:
+  created:
+  update:,
+}
+GABRIELA FRANCO - habilidades{}
+SANTIAGO PRADO MEDINA - token 
+{
+  key:"",
+  expired:"1h/4s/5day/1week",
+  status:
+  created:
+  update:
+  type:"jwt/socketio/session/base64"
+  devices:"web/movil/table/pwa"
+}
+SANTIAGO ANDRÉS RAMÍREZ CASTILLO - estudiantes
+{
+ name:"",
+ acudiente:{
+ { name:"",
+   age:23
+   type_parentes: "papá/mamá/siter/brother/grantmother"
+ }
+ grado:"",
+ number_room:"A3-9",
+  status:
+  created:
+  update:
+
+}
+JULY PAOLA RODRÍGUEZ ANGARITA - docentes 
+{
+  name:""
+  documento:
+  especialidad:[
+   { name: "", university:""}
+  ]
+   status:
+  created:
+  update:,
+  cnt_maestrias:
+  cnt_especializacin:"",
+  age:,
+  nacionalidad:
+}
+YERSON SUÁREZ ZUBIETA - sitios turisticos
+{
+ type: "playa/rio/cabañana/",
+ destino:"pianguita",
+ origen:"puerto de buenaventura"
+  status:
+  created:
+  update:,
+  habitaciones:[
+   { numero: 344, type: "vip/normal"}
+  ],
+  medios_de_transportes:[
+  { name: "cabotage/ burrita / canoa / lancha/ parapente/elicoptero/"}
+  ]
+}
+
