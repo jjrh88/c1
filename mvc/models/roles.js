0d48e38a69b5a44226b0a4696778134dfbe18ecb
andres.fandino <andresf084@gmail.com>
2021-11-03 19:48:34 -0500

    Update model roles

--- mvc/models/roles.js
+++ mvc/models/roles.js
@@ -1,23 +1,20 @@
 const mongoose = require('mongoose'),
 
 var RolesSchema = new mongoose.Schema
-({
+(
+    {
     role_id: { type: Number, required: true, index: true },
     role_name: { type: String, lowercase: true, required: true },
     role_active_status: { type: Boolean, default: false },
-    role_modules: [
+    role_modules:[ 
         {
-            module1: { type: Boolean, default: false },
-            module2: { type: Boolean, default: false },
-            module3: { type: Boolean, default: false },
-            module4: { type: Boolean, default: false },
-            module5: { type: Boolean, default: false }
+            modules: {type: String}
         }
-
     ],
     role_created_at: { type: Date, default: Date.now() },
     role_updated_at: { type: Date, default: Date.now() }
-})
+    }
+)
 
 
 module.exports = mongoose.model('mvc_roles', RolesSchema)
