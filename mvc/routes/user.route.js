2c2bec55e9105787cc547130934301ed5f47a5c7
andres.fandino <andresf084@gmail.com>
2021-11-03 19:14:10 -0500

    Actualizacion "user.enable"

--- mvc/routes/user.route.js
+++ mvc/routes/user.route.js
@@ -5,6 +5,7 @@ userCtrl = require('../controllers/user.controller')
 router.post('/', userCtrl.create)
 router.get("/", userCtrl.list);
 //router.put('/', userCtrl.update)
+router.post('/status', userCtrl.enable)
 router.delete('/:_id', userCtrl.delete)
 
 
