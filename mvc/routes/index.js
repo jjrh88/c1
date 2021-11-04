eec815c1ba4216ab286aed8a9cc2a3e488b10ca2
gfrancoa <gfrancoa@outlook.com>
2021-11-04 18:18:44 -0500

    habilidades routes y controller

--- mvc/routes/index.js
+++ mvc/routes/index.js
@@ -2,5 +2,6 @@ const { Router } = require('express'),
 router = Router()
 
 router.use('/user', require('../routes/user.route'))  
+router.use("/habilidad", require("../routes/habilidades.route"));  
 
 module.exports = router
